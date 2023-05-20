970b11d4d4ad6f446569d486392b58aecc3de1f4
mark <455711093@qq.com>
2023-05-21 02:34:08 +0800

    试试github

---
+++ packages/main/.umirc.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', component: '@/pages/index' },
+    { path: '/test', microApp: 'test' },
+  ],
+  fastRefresh: {},
+  qiankun: {
+    master: {
+      // 注册子应用信息
+      apps: [
+        {
+          name: 'test', // 唯一 id
+          entry: '//localhost:7666', // html entry
+        },
+      ],
+    },
+  },
+});
---
+++ packages/main/src/pages/index.tsx
@@ -0,0 +1,48 @@
+import { useState, useRef } from 'react';
+
+import VideoJS from './videoJS';
+const url = `https://stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4`;
+function VideoPlayer(props) {
+  const playerRef = useRef(null);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: url,
+        type: 'video/mp4',
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+      // videojs.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      // videojs.log('player will dispose');
+    });
+  };
+  return <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />;
+}
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <VideoPlayer />
+      <video width="640" height="480" controls>
+        <source src={url} type="video/mp4" />
+      </video>
+    </>
+  );
+}
+
+export default App;
